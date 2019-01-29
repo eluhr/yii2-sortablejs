19ad317581ad56ed711d50e01a4a69a58948b59d
Elias Luhr <e.luhr@herzogkommunikation.de>
2019-01-29 16:56:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# yii2-menu
+Yii2 Wrapper for [SortableJS/Sortable](https://github.com/SortableJS/Sortable)
+
+
+## Install
+
+```
+composer require eluhr/yii2-sortablejs
+```
---
+++ src/assets/SortableJsAssetBundle.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link http://www.diemeisterei.de/
+ * @copyright Copyright (c) 2019 diemeisterei GmbH, Stuttgart
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace eluhr\sortablejs\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class SortableJsAssetBundle
+ * @package eluhr\sortablejs\assets
+ * @author Elias Luhr <elias.luhr@gmail.com>
+ */
+class SortableJsAssetBundle extends AssetBundle
+{
+    public $sourcePath = '@vendor/npm-asset/sortablejs';
+
+    public $js = [
+        'Sortable.min.js'
+    ];
+}
